5fcb123269d58b65afc531cbce4a99acd8f81ad8
caitlinjaimee <caitlinbeaton929@gmail.com>
2026-05-30 18:47:06 +0200

    Add TaskItem class for task management

--- task.cs
+++ task.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace ChatbotWebForm
+{
+    public class TaskItem
+    {
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
